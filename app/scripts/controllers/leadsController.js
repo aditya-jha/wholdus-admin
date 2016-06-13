b27e74e2c81969fa1099c2331f2784e488eab0ea
Aishwarya Mittal <aishwaryamittal2011@gmail.com>
2016-06-13 16:42:56 +0530

    Added Status wise display of leads

--- app/scripts/controllers/leadsController.js
+++ app/scripts/controllers/leadsController.js
@@ -9,14 +9,13 @@
         'ngProgressBarService',
         'ToastService',
         '$location',
-        function($scope, $log, APIService, $routeParams, $rootScope, ngProgressBarService, ToastService, $location) {
+        function($scope, $log,APIService, $routeParams, $rootScope, ngProgressBarService, ToastService, $location) {
 
             $scope.data = {
                 leads: [],
                 leadID: null,
                 lead: {}
             };
-
             function getLeadType(){
                 if($location.url().substr(0,12)=='/buyer-leads')
                     $scope.leadType='buyerLeads';
@@ -37,23 +36,23 @@
                         if($scope.data.leadID) 
                             $scope.data.lead = response.buyer_leads[0];
                         else
-                             $scope.data.leads = response.buyer_leads;       
-                        }       
-                    else if($scope.leadType=='sellerLeads' && response.seller_leads.length>0) {
-                        if($scope.data.leadID) 
-                            $scope.data.lead = response.seller_leads[0];
-                        else
-                             $scope.data.leads = response.seller_leads;       
-                        }
-                        else if($scope.leadType=='contactusLeads' && response.contactus_leads.length>0) {
-                        if($scope.data.leadID) 
-                            $scope.data.lead = response.contactus_leads[0];
-                        else
-                             $scope.data.leads = response.contactus_leads;       
-                        }       
-                        
-                     else if($scope.data.leadID) {
-                        ToastService.showActionToast("No such lead exists! GO BACK", 0)
+                           $scope.data.leads = response.buyer_leads;       
+                   }       
+                   else if($scope.leadType=='sellerLeads' && response.seller_leads.length>0) {
+                    if($scope.data.leadID) 
+                        $scope.data.lead = response.seller_leads[0];
+                    else
+                       $scope.data.leads = response.seller_leads;       
+                    }
+                    else if($scope.leadType=='contactusLeads' && response.contactus_leads.length>0) {
+                    if($scope.data.leadID) 
+                    $scope.data.lead = response.contactus_leads[0];
+                    else
+                   $scope.data.leads = response.contactus_leads;       
+                    }       
+
+                    else if($scope.data.leadID) {
+                    ToastService.showActionToast("No such lead exists! GO BACK", 0)
                         .then(function(response) {
                             if($scope.leadType=='buyerLeads')
                                 $location.url('/buyer-leads');
@@ -62,11 +61,20 @@
                             else if($scope.leadType=='contactusLeads')
                                 $location.url('/contactus-leads');
 
-                        });
+                    });
                     }
-                }, function(error) {
-                    $rootScope.$broadcast('endProgressbar');
-                });
+                    else{
+
+                         if($scope.leadType=='buyerLeads')
+                                 $scope.data.leads = response.buyer_leads;
+                            else if($scope.leadType=='sellerLeads')        
+                                $scope.data.leads = response.seller_leads;
+                            else if($scope.leadType=='contactusLeads')
+                                $scope.data.leads = response.contactus_leads; 
+                    }
+    }, function(error) {
+        $rootScope.$broadcast('endProgressbar');
+    });
             }
 
             function pageSetting() {
@@ -77,7 +85,7 @@
                             buyerleadID: $routeParams.buyerleadID
                         });
                     } else {
-                        getLeads();
+                            getLeads();
                     }
                 } 
                 else if($scope.leadType=='sellerLeads'){
@@ -88,49 +96,57 @@
                         });
                     } else {
                         getLeads();
-                    }
-                } if($scope.leadType=='contactusLeads'){
-                    if($routeParams.contactusleadID) {
-                        $scope.data.leadID = parseInt($routeParams.contactusleadID);
-                        getLeads({
-                            contactusleadID: $routeParams.contactusleadID
-                        });
-                    } else {
-                        getLeads();
-                    }
                 }
-            }
-
-            pageSetting();
-
-            $scope.reset = function() {
-                pageSetting();
-            };
-
-            $scope.changeLeads = function(event, type) {
-                if(type=="DELETE" || type=="PUT") {
-                    $rootScope.$broadcast('showProgressbar');
-                    APIService.apiCall(type, APIService.getAPIUrl($scope.leadType), $scope.data.lead)
-                    .then(function(response) {
-                        $rootScope.$broadcast('endProgressbar');
-                        pageSetting();
-                        ToastService.showActionToast("successful", 0).then(function(response) {
-                            if(type=="DELETE") {
-                                if($scope.leadType=='buyerLeads')
-                                    $location.url('/buyer-leads');
-                                else if($scope.leadType=='sellerLeads')        
-                                    $location.url('/seller-leads');
-                                else if($scope.leadType=='contactusLeads')
-                                    $location.url('/contactus-leads');
-                            }
-                        });
-                    }, function(error) {
-                        $rootScope.$broadcast('endProgressbar');
-                        ToastService.showActionToast("something went wrong! please reload", 0);
+            } if($scope.leadType=='contactusLeads'){
+                if($routeParams.contactusleadID) {
+                    $scope.data.leadID = parseInt($routeParams.contactusleadID);
+                    getLeads({
+                        contactusleadID: $routeParams.contactusleadID
                     });
-                }
-            };
-
+                } else {
+                    getLeads();
+            }
         }
-        ]);
+    }
+
+    pageSetting();
+
+    $scope.resolvedLeads=function(){
+        getLeads({status: 1});
+
+    }
+    $scope.newLeads=function(){
+        getLeads({status: 0});
+
+    }
+    $scope.reset = function() {
+        pageSetting();
+    };
+
+    $scope.changeLeads = function(event, type) {
+        if(type=="DELETE" || type=="PUT") {
+            $rootScope.$broadcast('showProgressbar');
+            APIService.apiCall(type, APIService.getAPIUrl($scope.leadType), $scope.data.lead)
+            .then(function(response) {
+                $rootScope.$broadcast('endProgressbar');
+                pageSetting();
+                ToastService.showActionToast("successful", 0).then(function(response) {
+                    if(type=="DELETE") {
+                        if($scope.leadType=='buyerLeads')
+                            $location.url('/buyer-leads');
+                        else if($scope.leadType=='sellerLeads')        
+                            $location.url('/seller-leads');
+                        else if($scope.leadType=='contactusLeads')
+                            $location.url('/contactus-leads');
+                    }
+                });
+            }, function(error) {
+                $rootScope.$broadcast('endProgressbar');
+                ToastService.showActionToast("something went wrong! please reload", 0);
+            });
+        }
+    };
+
+}
+]);
 })();
