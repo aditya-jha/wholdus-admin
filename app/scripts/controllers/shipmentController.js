5e40ae62b7702b1701b1cd0c8abc0069e9ffa89a
Shubham <shubhamgupta.3101994@gmail.com>
2016-06-16 14:49:00 +0530

    shipment page track status

--- app/scripts/controllers/shipmentController.js
+++ app/scripts/controllers/shipmentController.js
@@ -9,12 +9,57 @@
         'ngProgressBarService',
         'ToastService',
         '$location',
-        function($scope, $log, APIService, $routeParams, $rootScope, ngProgressBarService, ToastService, $location){
+        'UtilService',
+        function($scope, $log, APIService, $routeParams, $rootScope, ngProgressBarService, ToastService, $location, UtilService){
 
             $scope.data = {
                 shipments: [],
                 shipmentID: null,
                 shipment: {}
+            };
+
+            $scope.state = [
+            {
+                display_value: "3PL manifested",
+                value: 3,
+                active : false
+            },
+            {
+                display_value: "3PL In Transit",
+                value: 4,
+                active : false
+            },
+            {
+                display_value: "Stuck In Transit",
+                value: 5,
+                active : false
+            },
+            {
+                display_value: "Delivered",
+                value: 6,
+                active : false
+            },
+            {
+                display_value: "RTO In Transit",
+                value: 7,
+                active : false
+            },
+            {
+                display_value: "RTO Delivered",
+                value: 8,
+                active : false
+            },
+            {
+                display_value: "Lost",
+                value: 9,
+                active : false
+
+            }
+            ];
+
+            $scope.change ={
+                status: 3,
+                ordershipmentID: null
             };
 
             function getShipments(params){
@@ -25,6 +70,9 @@
                     if(response.order_shipments.length){
                         if($scope.data.shipmentID){
                             $scope.data.shipment = response.order_shipments[0];
+                            $scope.change.status = $scope.data.shipment.status.value;
+                            $scope.change.ordershipmentID = $scope.data.shipmentID;
+                            statusAvailable();
                         }
                         else{
                             $scope.data.shipments = response.order_shipments;
@@ -39,6 +87,8 @@
                     $rootScope.$broadcast('endProgressbar');
                     ToastService.showSimpleToast("Something went wrong. Try again", 3000);
                 })
+
+                
             };
 
             function pagesetting(){
@@ -55,6 +105,41 @@
 
             pagesetting();
 
+            function statusAvailable(){
+                for (var i = 0; i <7 ; i++) {
+                    $scope.state[i].active =true;
+                    }
+
+                switch($scope.data.shipment.status.value){
+                    case 3:
+                        $scope.state[1].active = false;
+                        break;
+                    case 4:
+                        $scope.state[2].active = false;
+                        $scope.state[3].active = false;
+                        $scope.state[4].active = false;
+                        $scope.state[6].active = false;
+                        break;
+                    case 5:
+                        $scope.state[1].active = false;
+                        $scope.state[3].active = false;
+                        $scope.state[4].active = false;
+                        $scope.state[6].active = false;
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        $scope.state[1].active = false;
+                        $scope.state[5].active = false;
+                        $scope.state[6].active = false;
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;                        
+                }
+            }
+
             $scope.totalAmount=function(){
                 var total = 0;
                 for(var i = 0 ;i < $scope.data.shipment.order_items.length; i++){
@@ -62,6 +147,25 @@
                 }
                 return total;
             }
+
+            $scope.changeStatus = function(event, index){
+                var temp = $scope.data.shipment.status.display_value;
+                $scope.change.status = $scope.state[index].value;
+                  $rootScope.$broadcast('showProgressbar');
+                    APIService.apiCall("PUT", APIService.getAPIUrl("ordershipment"), $scope.change)
+                        .then(function(response) {
+                            $scope.data.shipment.status.display_value = $scope.state[index].display_value;
+                            $scope.data.shipment.status.value = $scope.state[index].value;
+                            $rootScope.$broadcast('endProgressbar');
+                            ToastService.showSimpleToast("Status Changed", 2000);
+                            statusAvailable();
+                },function(error){
+                    $rootScope.$broadcast('endProgressbar');
+                    ToastService.showSimpleToast("Something went wrong. Try again", 3000);
+                    $scope.data.shipment.status.display_value = temp;
+                })
+
+             };
 
         }
         ]);
