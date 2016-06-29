18ea127d500b90284be6cb2f3e5e01c290b774a5
Aishwarya Mittal <aishwaryamittal2011@gmail.com>
2016-06-29 17:41:11 +0530

    Added download filter file button on products page

--- app/scripts/controllers/productController.js
+++ app/scripts/controllers/productController.js
@@ -12,7 +12,7 @@
         'UtilService',
         'ConstantKeyValueService',
         function($scope, $log, APIService, $routeParams, $rootScope,$location, ngProgressBarService,
-         ToastService, UtilService, ConstantKeyValueService) {
+           ToastService, UtilService, ConstantKeyValueService) {
             $scope.data = {
                 products: [],
                 productID: null,
@@ -87,46 +87,46 @@
 
                         else {
                             if(response.total_pages > 1) {
-                               $scope.settings.enablePagination = true;
-                               $rootScope.$broadcast('setPage', {
+                             $scope.settings.enablePagination = true;
+                             $rootScope.$broadcast('setPage', {
                                 page: $scope.settings.page,
                                 totalPages: Math.ceil(response.total_products/$scope.settings.itemsPerPage)
-                                }); 
+                            }); 
+                         }
+                         else{
+                            $scope.settings.enablePagination = false;
+                        }
+                        angular.forEach(response.products, function(value, key) {
+                            value.images = UtilService.getImages(value);
+                            if(value.images.length){
+                                value.imageUrl = UtilService.getImageUrl(value.images[0], '200x200');
                             }
                             else{
-                                $scope.settings.enablePagination = false;
-                             }
-                            angular.forEach(response.products, function(value, key) {
-                                value.images = UtilService.getImages(value);
-                                if(value.images.length){
-                                    value.imageUrl = UtilService.getImageUrl(value.images[0], '200x200');
-                                }
-                                else{
-                                    value.imageUrl = 'images/200.png';
-                                }
-                            });
-                            $scope.data.products = response.products;
+                                value.imageUrl = 'images/200.png';
+                            }
+                        });
+                        $scope.data.products = response.products;
                         
-                       
-                       }
-                   }
-                   else if($scope.data.productID) {
+
+                    }
+                }
+                else if($scope.data.productID) {
                     ToastService.showActionToast("No such product exists! GO BACK", 0)
                     .then(function(response) {
                         $location.url('/products');
                     });
-                    }
-                    else{
-                        $scope.data.products=[];
-                        $scope.settings.enablePagination = false;
-                    }
+                }
+                else{
+                    $scope.data.products=[];
+                    $scope.settings.enablePagination = false;
+                }
             }, function(error) {
                 $rootScope.$broadcast('endProgressbar');
             });
             }
 
             function pageSetting() {
-      
+
                 if($routeParams.productID) {
                     $scope.data.productID = parseInt($routeParams.productID);
                     getproducts({
@@ -134,20 +134,20 @@
                     });
                 } 
                 else {
-                   $scope.categoryString=UtilService.categoryString;
-                   $scope.sellerString= UtilService.sellerString;
-                   $scope.minPrice= UtilService.minPrice;
-                   $scope.maxPrice= UtilService.maxPrice;
-                   $scope.fabric= UtilService.fabric;
-                   $scope.colour=UtilService.colour;
-                   if(UtilService.categoryString){
-                        $scope.selectedCategories=UtilService.categoryString.split(',');
-                   }
-                   if(UtilService.sellerString){
-                        $scope.selectedSellers=UtilService.sellerString.split(',');
-                   }
-                   getproducts(
-                   {
+                 $scope.categoryString=UtilService.categoryString;
+                 $scope.sellerString= UtilService.sellerString;
+                 $scope.minPrice= UtilService.minPrice;
+                 $scope.maxPrice= UtilService.maxPrice;
+                 $scope.fabric= UtilService.fabric;
+                 $scope.colour=UtilService.colour;
+                 if(UtilService.categoryString){
+                    $scope.selectedCategories=UtilService.categoryString.split(',');
+                }
+                if(UtilService.sellerString){
+                    $scope.selectedSellers=UtilService.sellerString.split(',');
+                }
+                getproducts(
+                {
                     categoryID: $scope.categoryString,
                     sellerID:$scope.sellerString,
                     items_per_page:$scope.settings.itemsPerPage,
@@ -158,60 +158,81 @@
                     colour:$scope.colour
                     
                 });
-               }
-           }
+            }
+        }
 
 
-           pageSetting();
+        pageSetting();
 
-         $scope.reset = function() {
-          
-          
+        $scope.reset = function() {
+
+
             pageSetting();
         };
 
         $scope.reload=function(){
             UtilService.setCategoryString('');
-           UtilService.setSellerString('');
-           UtilService.setProp(null,null,null,null);
+            UtilService.setSellerString('');
+            UtilService.setProp(null,null,null,null);
             $scope.selectedCategories=null;
             $scope.selectedSellers=null;
-              $location.url('/products');
+            $location.url('/products');
         };
 
-    
+
         $scope.filterProducts=function(){
-           UtilService.setCategoryString($scope.selectedCategories.toString());
-           UtilService.setSellerString($scope.selectedSellers.toString());
-           UtilService.setProp($scope.minPrice,$scope.maxPrice,$scope.fabric,$scope.colour);
-           $scope.settings.page=1;
-           $location.search('page', 1);
-           pageSetting();
-        };
+         UtilService.setCategoryString($scope.selectedCategories.toString());
+         UtilService.setSellerString($scope.selectedSellers.toString());
+         UtilService.setProp($scope.minPrice,$scope.maxPrice,$scope.fabric,$scope.colour);
+         $scope.settings.page=1;
+         $location.search('page', 1);
+         pageSetting();
+     };
 
-        $scope.changeProduct = function(type,hide) {
-         $rootScope.$broadcast('showProgressbar');
-         if(hide){
-            $scope.data.product.show_online=false;}
-            APIService.apiCall(type, APIService.getAPIUrl("products"), $scope.data.product)
-            .then(function(response) {
-                $rootScope.$broadcast('endProgressbar');
-                pageSetting();
-                ToastService.showActionToast("successful", 0).then(function(response) {
-                 if(type=="DELETE"){
-                    $location.url('/products');
-                }
+     $scope.downloadFile=function(){
+         $scope.filterProducts();
+         var paramsString='';
+         if($scope.categoryString.length>0)
+            paramsString+='&categoryID='+$scope.categoryString;  
+        if($scope.sellerString.length>0)
+            paramsString+='&sellerID='+$scope.sellerString;  
+        if($scope.minPrice!=null)
+            paramsString+='&min_price_per_unit='+$scope.minPrice;  
+        if($scope.maxPrice!=null)
+            paramsString+='&max_price_per_unit='+$scope.maxPrice;  
+        if($scope.fabric!=null)
+            paramsString+='&fabric='+$scope.fabric;
+        if($scope.colour!=null)
+            paramsString+='&colour='+$scope.colour;
 
-            });
-            }, function(error) {
-                $rootScope.$broadcast('endProgressbar');
-                ToastService.showActionToast("something went wrong! please reload", 0);
-            });
-        };
+       
+        window.open(ConstantKeyValueService.apiBaseUrl+'products/generatefile/?'+paramsString);
 
-        $scope.clearSearchTerm = function() {
-            $scope.searchTerm = '';
-        };
+   }
+
+   $scope.changeProduct = function(type,hide) {
+       $rootScope.$broadcast('showProgressbar');
+       if(hide){
+        $scope.data.product.show_online=false;}
+        APIService.apiCall(type, APIService.getAPIUrl("products"), $scope.data.product)
+        .then(function(response) {
+            $rootScope.$broadcast('endProgressbar');
+            pageSetting();
+            ToastService.showActionToast("successful", 0).then(function(response) {
+               if(type=="DELETE"){
+                $location.url('/products');
+            }
+
+        });
+        }, function(error) {
+            $rootScope.$broadcast('endProgressbar');
+            ToastService.showActionToast("something went wrong! please reload", 0);
+        });
+    };
+
+    $scope.clearSearchTerm = function() {
+        $scope.searchTerm = '';
+    };
 
 
 
